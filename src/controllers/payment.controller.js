dffc0fc98838c94c0b660fbce9dfd331f38257f8
jobnovember <pawat-ka@rmutp.ac.th>
2021-02-20 10:54:39 +0700

    create id at payment

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -1,44 +1,43 @@
-const HttpException = require('../utils/HttpException.utils')
-const PaymentModel = require('../models/payment.model')
+const HttpException = require("../utils/HttpException.utils");
+const PaymentModel = require("../models/payment.model");
+const CreateID = require("../utils/CreateID");
 
-class PaymentController{
-    table = "payment"
+class PaymentController {
+  table = "payment";
 
-    getAll = async(req, res, next) => {
-        const list = await PaymentModel.find()
-        if(list.length == 0)
-            throw new HttpException(404, `Not found any ${this.table}`)
-        res.status(206).send(list) 
-    }         
+  getAll = async (req, res, next) => {
+    const list = await PaymentModel.find();
+    if (list.length == 0)
+      throw new HttpException(404, `Not found any ${this.table}`);
+    res.status(206).send(list);
+  };
 
-    getById = async(req, res, next) => {
-        const result = await PaymentModel.findOne({payment_id: req.params.id}) 
-        if(!result)
-            throw new HttpException(404, `Not found any ${this.table}`)
-        res.status(206).send(result)
-    }
+  getById = async (req, res, next) => {
+    const result = await PaymentModel.findOne({ payment_id: req.params.id });
+    if (!result) throw new HttpException(404, `Not found any ${this.table}`);
+    res.status(206).send(result);
+  };
 
-    update = async(req, res, next) => {
-        const result = await PaymentModel.update(req.body, req.body.id)
-        if(!result)
-            throw new HttpException(404, 'Something went wrong')
-        res.staus(200).send(`${this.table} was edited`)
-    }
+  update = async (req, res, next) => {
+    const result = await PaymentModel.update(req.body, req.body.id);
+    if (!result) throw new HttpException(404, "Something went wrong");
+    res.staus(200).send(`${this.table} was edited`);
+  };
 
-    delete = async(req, res, next) => {
-        const result = await PaymentModel.delete({payment_id: req.params.id})
-        if(!result)
-            throw new HttpException(404, `${this.table} not found`)
-        res.staus(200).send(`${this.table} was deleted`)
-    }
+  delete = async (req, res, next) => {
+    const result = await PaymentModel.delete({ payment_id: req.params.id });
+    if (!result) throw new HttpException(404, `${this.table} not found`);
+    res.staus(200).send(`${this.table} was deleted`);
+  };
 
-    create = async(req, res, next) => {
-        const result = await PaymentModel.create(req.body)
-        if(!result)
-            throw new HttpException(404, 'Something went wrong')
-        res.status(200).send(`${this.table} was added`)
-    }
-
+  create = async (req, res, next) => {
+    req.body.payment_id = (await CreateID.hash(req.body))
+      .toString()
+      .replace("/", "");
+    const result = await PaymentModel.create(req.body);
+    if (!result) throw new HttpException(404, "Something went wrong");
+    res.status(200).send(`${this.table} was added`);
+  };
 }
 
-module.exports = new PaymentController
+module.exports = new PaymentController();
--- src/models/payment.model.js
+++ src/models/payment.model.js
@@ -1,47 +1,45 @@
-const query = require('../db/connect')
-const pairSQL = require('../utils/pairSQL')
+const query = require("../db/connect");
+const pairSQL = require("../utils/pairSQL");
 
-class PaymentModel{
+class PaymentModel {
+  table = "payment";
 
-    table = 'payment'
+  find = async (params = {}) => {
+    const keys = Object.keys(params);
+    let sql = `SELECT * FROM ${this.table}`;
+    if (keys.length == 0) return await query(sql);
+    const { columns, values } = pairSQL(params);
+    sql += ` WHERE ${columns}`;
+    return await query(sql, [...values]);
+  };
 
-    find = async(params = {}) => {
-        const keys = Object.keys(params) 
-        let sql = `SELECT * FROM ${this.table}`
-        if(keys.length == 0)
-            return await query(sql)
-        const {columns, values} = pairSQL(params)    
-        sql += ` WHERE ${columns}`
-        return await query(sql, [...values])
-    }
-    
-    findOne = async(params) => {
-        const {columns, values} = pairSQL(params)
-        const sql = `SELECT * FROM ${this.table} WHERE ${columns}`
-        const result = await  query(sql, [...values])
-        return result[0]
-    }
+  findOne = async (params) => {
+    const { columns, values } = pairSQL(params);
+    const sql = `SELECT * FROM ${this.table} WHERE ${columns}`;
+    const result = await query(sql, [...values]);
+    return result[0];
+  };
 
-    create = async({type, picture_url}) => {
-        const sql = `INSERT INTO ${this.table} (type, picture_url) VALUES (?,?)`
-        const result = await query(sql, [type, picture_url])
-        const affectedRows = result ? result.affectedRows : 0
-        return affectedRows
-    }
+  create = async ({ payment_id, type, picture_url }) => {
+    const sql = `INSERT INTO ${this.table} (payment_id, type, picture_url) VALUES (?,?,?)`;
+    const result = await query(sql, [payment_id, type, picture_url]);
+    const affectedRows = result ? result.affectedRows : 0;
+    return affectedRows;
+  };
 
-    update = async(params, id) => {
-        const {columns, values} = pairSQL(params)
-        const sql = `UPDATE ${this.table} SET ${columns} WHERE payment_id = ?`
-        const result = await query(sql, [...values, id])
-        return result
-    }
+  update = async (params, id) => {
+    const { columns, values } = pairSQL(params);
+    const sql = `UPDATE ${this.table} SET ${columns} WHERE payment_id = ?`;
+    const result = await query(sql, [...values, id]);
+    return result;
+  };
 
-    delete = async(id) => {
-        const sql = `DELETE FROM ${this.table} WHERE payment_id = ?`
-        const result = await query(sql, [id])
-        const affectedRows = result ? result.affectedRows : 0
-        return affectedRows
-    }
+  delete = async (id) => {
+    const sql = `DELETE FROM ${this.table} WHERE payment_id = ?`;
+    const result = await query(sql, [id]);
+    const affectedRows = result ? result.affectedRows : 0;
+    return affectedRows;
+  };
 }
 
-module.exports = new PaymentModel 
+module.exports = new PaymentModel();
