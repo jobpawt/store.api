d781384743023e9764bd890a239947db0a29a3d4
jobnovember <pawat-ka@rmutp.ac.th>
2021-02-20 10:56:32 +0700

    create id at promotion

--- src/controllers/promotion.controller.js
+++ src/controllers/promotion.controller.js
@@ -1,48 +1,44 @@
-const HttpException = require('../utils/HttpException.utils')
-const PromotionModel = require('../models/promotion.model')
-const CreateID = require('../utils/CreateID')
+const HttpException = require("../utils/HttpException.utils");
+const PromotionModel = require("../models/promotion.model");
+const CreateID = require("../utils/CreateID");
 
-class PromotionController{
+class PromotionController {
+  table = "promotion";
 
-    table = "promotion"
+  getAll = async (req, res, next) => {
+    const list = await PromotionModel.find();
+    if (list.length == 0)
+      throw new HttpException(404, `Not found any ${this.table}`);
+    res.status(206).send(list);
+  };
 
-    getAll = async(req, res, next) => {
-        const list = await PromotionModel.find()
-        if(list.length == 0)
-            throw new HttpException(404, `Not found any ${this.table}`)
-        res.status(206).send(list) 
-    }         
+  getById = async (req, res, next) => {
+    const result = await PromotionModel.findOne({ pro_id: req.params.id });
+    if (!result) throw new HttpException(404, `Not found any ${this.table}`);
+    res.status(206).send(result);
+  };
 
-    getById = async(req, res, next) => {
-        const result = await PromotionModel.findOne({pro_id: req.params.id}) 
-        if(!result)
-            throw new HttpException(404, `Not found any ${this.table}`)
-        res.status(206).send(result)
-    }
+  update = async (req, res, next) => {
+    const result = await PromotionModel.update(req.body, req.body.id);
+    if (!result) throw new HttpException(404, "Something went wrong");
+    res.staus(200).send(`${this.table} was edited`);
+  };
 
-    update = async(req, res, next) => {
-        const result = await PromotionModel.update(req.body, req.body.id)
-        if(!result)
-            throw new HttpException(404, 'Something went wrong')
-        res.staus(200).send(`${this.table} was edited`)
-    }
+  delete = async (req, res, next) => {
+    const result = await PromotionModel.delete({ pro_id: req.params.id });
+    if (!result) throw new HttpException(404, `${this.table} not found`);
+    res.staus(200).send(`${this.table} was deleted`);
+  };
 
-    delete = async(req, res, next) => {
-        const result = await PromotionModel.delete({pro_id: req.params.id})
-        if(!result)
-            throw new HttpException(404, `${this.table} not found`)
-        res.staus(200).send(`${this.table} was deleted`)
-    }
-
-    create = async(req, res, next) => {
-        const id = await CreateID.hash(req.body)
-        req.body.pro_id = id
-        const result = await PromotionModel.create(req.body)
-        if(!result)
-            throw new HttpException(404, 'Something went wrong')
-        res.status(200).send(`${this.table} was added`)
-    }
-
+  create = async (req, res, next) => {
+    const id = (await CreateID.hash(req.body))
+      .toString()
+      .replace("/", "");
+    req.body.pro_id = id;
+    const result = await PromotionModel.create(req.body);
+    if (!result) throw new HttpException(404, "Something went wrong");
+    res.status(200).send(`${this.table} was added`);
+  };
 }
 
-module.exports = new PromotionController 
+module.exports = new PromotionController();
